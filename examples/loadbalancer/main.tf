03b7297948a7905e0f3a9fa31ae98116736109bf
Kelly Ferrone <kelly@duplocloud.net>
2025-03-03 12:13:46 -0700

    example for lb

---
+++ examples/loadbalancer/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.4.4"
+  required_providers {
+    duplocloud = {
+      source  = "duplocloud/duplocloud"
+      version = ">= 0.9.40"
+    }
+  }
+  # backend "s3" {
+  #   workspace_key_prefix = "duplocloud/components"
+  #   key                  = "micro-service"
+  #   encrypt              = true
+  # }
+}
+provider "duplocloud" {}
+
+module "tenant" {
+  source = "../../modules/loadbalancer"
+  name   = "mylb"
+  tenant = terraform.workspace
+  class  = "standalone-alb"
+}
