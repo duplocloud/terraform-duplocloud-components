52638fbdea2d47fa29619d0c6926765bf6a063cf
Kelly Ferrone <kelly@duplocloud.net>
2025-03-03 12:32:10 -0700

    only do lb in this pr

--- modules/tenant/main.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  tenant_id = duplocloud_tenant.this.tenant_id
-  infra     = data.duplocloud_infrastructure.this
-}
-
-data "duplocloud_infrastructure" "this" {
-  infra_name = var.infra_name
-}
