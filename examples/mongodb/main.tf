09b7cc4087494e0965a452a900dc75d5c117939a
Tanmay Kumar Pandey <tanmay.pandey@duplocloud.net>
2025-03-05 15:05:21 +0530

    Added example

---
+++ examples/mongodb/main.tf
@@ -0,0 +1,56 @@
+# Specify the required Terraform version for compatibility
+terraform {
+  required_version = "~> 1.10.5"
+
+  # Specify required providers and their versions
+  required_providers {
+    mongodbatlas = {
+      source  = "mongodb/mongodbatlas",
+      version = "1.24.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5.0"
+    }
+
+  }
+}
+
+
+# Use the secrets in the MongoDB Atlas provider
+provider "mongodbatlas" {
+  public_key  = "mongodb_atlas_public_key"
+  private_key = "mongodb_atlas_private_key"
+}
+
+provider "random" {}
+
+module "mongo_atlas" {
+  source                 = "../../modules/mongodb"
+  cluster_name           = "my-mongodb-cluster"
+  cluster_type           = "REPLICASET"
+  environment            = "dev"
+  mongo_db_major_version = 7
+  mongo_project_id       = "my-project-id"
+  region_configs = {
+    electable_specs = {
+      instance_size = "M10"
+      node_count    = 3
+      disk_size_gb  = 10
+    }
+    provider_name = "AWS"
+    priority      = 7
+    region_name   = "US_EAST_1"
+  }
+  mongo_user_details = {
+    username = "myuser"
+    roles = {
+      database_name = "admin"
+      role_name     = "readWrite"
+    }
+    auth_database_name = "admin"
+  }
+  mongodb_atlas_public_key  = "my-public-key"
+  mongodb_atlas_private_key = "my-private-key"
+
+}
