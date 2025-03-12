b0fd18b27d247b935267a5cde1923c9391ba4ab2
Kelly Ferrone <kelly@duplocloud.net>
2025-03-12 13:16:03 -0600

    moved aws tenant

--- modules/tenant-aws/main.tf
+++ modules/tenant-aws/main.tf
@@ -1,12 +1,12 @@
 module "tenant" {
-  source = "../" 
+  source = "../tenant" 
   infra_name = var.infra_name
   name = var.name
   settings = var.settings
 }
 
 module "iam" {
-  source = "../../tenant-role-extension"
+  source = "../tenant-role-extension"
   tenant_name = var.name 
   policy_name = "${var.name}-custom"
   iam_policy_json = var.policy
