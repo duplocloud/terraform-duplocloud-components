ab0c070bcfe2e9c602ced3269db54101013e6975
Kelly Ferrone <kelly@duplocloud.net>
2025-05-22 11:59:11 -0600

    started adding the rules

--- modules/project-gh/workflows.tf
+++ modules/project-gh/workflows.tf
@@ -1,3 +1,70 @@
+locals {
+  ## 
+  # Global variables for all of the workflows
+  ##
+  workflow_context = {
+    name    = var.name
+    ref     = "main"
+    cloud   = var.cloud
+    props   = local.props
+    use_app = false
+  }
+  ##
+  # The pre-made by Duplocloud workflows and templates. 
+  # The content for these are the workflows in the workflows directory.
+  # These can be simply enabled or disabled and user can even override them. 
+  ## 
+  default_workflows = {
+    image = {
+      enabled    = true
+      content    = null
+      conditions = local.condition.service
+      context    = {}
+    }
+    update_image = {
+      enabled    = true
+      conditions = local.condition.service
+      content    = null
+      context    = {}
+    }
+  }
+  default_workflow_keys = keys(local.default_workflows)
+  ##
+  # First merge the user provided workflows and the default workflows
+  # This will cause the conditions to be nulled out if the user updated 
+  # the workflow values. We need the default conditions back when this happens. 
+  # If it's some user defined workflow with null conditions then empty object it is
+  ## 
+  workflows = {
+    for name, workflow in merge(local.default_workflows, var.workflows) : name => merge(
+      workflow,
+      {
+        conditions = (
+          contains(local.default_workflow_keys, name) &&
+          workflow.conditions == null
+        ) ? local.default_workflows[name].conditions : coalesce(workflow.conditions, {})
+      }
+    )
+  }
+  ##
+  # This is basically saying for each of the workflows;
+  # first check if the repo has any properties that exist in the condition
+  # If so, then make sure the actual value is one of the allowed values in the condition
+  # Then we can say it's active
+  ## 
+  active_workflows = [
+    for name, workflow in local.workflows : name
+    if workflow.enabled && anytrue([
+      for k, allowed in workflow.conditions : anytrue([
+        for v in lookup(local.props, k, []) : contains(allowed, v)
+      ])
+    ])
+  ]
+}
+
+##
+# Builds the actual file and commits it to the designated repo. 
+## 
 resource "github_repository_file" "workflows" {
   for_each = {
     for name in local.active_workflows : name => local.workflows[name]
@@ -17,7 +84,7 @@ resource "github_repository_file" "workflows" {
         },                                                                   # specific to the loop
         local.workflow_context,                                              # the globals
         lookup(local.default_workflows, each.key, { context = {} }).context, # the defaults
-        lookup(local.workflows, each.key, { context = {} }).context,         # the user inputs
+        coalesce(each.value.context, {})                                     # the user input
       )
     )
   )
