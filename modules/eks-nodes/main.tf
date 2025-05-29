bbb076955235237391e904261b0530d13d52e190
Zachary Chism <zachchismdc@gmail.com>
2025-05-29 10:27:46 -0400

    Add 3 minute timeout to handle scale-to-0

--- modules/eks-nodes/main.tf
+++ modules/eks-nodes/main.tf
@@ -64,6 +64,14 @@ resource "duplocloud_aws_launch_template" "nodes" {
   version             = 1
   version_description = data.aws_ami.ami.description
   ami                 = local.asg_ami
+
+  depends_on = [ time_sleep.wait_3_minutes ]
+}
+
+resource "time_sleep" "wait_3_minutes" {
+  depends_on = [duplocloud_asg_profile.nodes]
+
+  create_duration = "240s"
 }
 
 resource "duplocloud_asg_profile" "nodes" {
