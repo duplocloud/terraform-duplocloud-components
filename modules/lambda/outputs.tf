7e32aff2328918e27ef623b2ccaa236c2db10e97
Kelly Ferrone <kelly@duplocloud.net>
2025-09-15 13:08:41 -0600

    function outputs

---
+++ modules/lambda/outputs.tf
@@ -0,0 +1,14 @@
+output "name" {
+  description = "The name of the Lambda function"
+  value       = aws_lambda_function.this.name
+}
+
+output "fullname" {
+  description = "The full name of the Lambda function"
+  value       = aws_lambda_function.this.fullname
+}
+
+output "arn" {
+  description = "The ARN of the Lambda function"
+  value       = aws_lambda_function.this.arn
+}
